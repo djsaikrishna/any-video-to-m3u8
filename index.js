de2d55f772ea4b326f3d2f9c4c77cb7acfae2273
drahulroy <RAHULTHEROY47@GMAIL.COM>
2023-08-12 01:47:43 +0530

    update

--- index.js
+++ index.js
@@ -3,24 +3,32 @@ const multer = require('multer');
 const { exec } = require('child_process');
 
 const app = express();
-const port = 3000;
+const port = 3020;
 
-const storage = multer.memoryStorage(); // for demo purposes we're not saving to disk
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads/')
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + '-' + file.originalname)
+    }
+  });
+  
 const upload = multer({ storage: storage });
+  
 
 app.get('/', (req, res) => {
     res.send('Upload your video to convert it to m3u8');
 });
+
 
 app.post('/upload', upload.single('video'), (req, res) => {
     if (!req.file) {
         return res.status(400).send('No video uploaded.');
     }
 
-    const inputVideoPath = 'input.mp4';  // Name it based on your requirement
-    const outputVideoPath = 'output.m3u8';
-
-    require('fs').writeFileSync(inputVideoPath, req.file.buffer);
+    const inputVideoPath = req.file.path; 
+    const outputVideoPath = 'output-' + Date.now() + '.m3u8';
 
     const ffmpegCommand = `ffmpeg -i ${inputVideoPath} -profile:v baseline -level 3.0 -s 640x360 -start_number 0 -hls_time 10 -hls_list_size 0 -f hls ${outputVideoPath}`;
 
@@ -29,9 +37,8 @@ app.post('/upload', upload.single('video'), (req, res) => {
             console.error(`exec error: ${error}`);
             return res.status(500).send('Error during conversion.');
         }
-
-        res.send('Video converted successfully!');
-        // Optionally, you can send the m3u8 file or its content as a response.
+        // You can implement a mechanism to serve the .m3u8 file or notify the user when it's done.
+        res.send('Video is being converted. Please check back later.');
     });
 });
 
